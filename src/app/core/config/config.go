5a20a3eed34da33765173f1cace6aeb93e77c444
Berlin <berlinsantoscruz@gmail.com>
2023-01-22 23:32:29 -0400

    added reproductoras projection table

--- src/app/core/config/config.go
+++ src/app/core/config/config.go
@@ -1,6 +1,6 @@
 package config
 
-var VERSION string = "0.0.4"
+var VERSION string = "0.0.5"
 var DatabaseName string = "dataprod"
 var DatabaseUri string = "mongodb://localhost:27017/"
 var IsBench bool = true
